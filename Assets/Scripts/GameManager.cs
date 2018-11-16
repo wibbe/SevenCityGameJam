6694d93afce730904f4f997cadf4c16a7b52f6d9
Daniel Wiberg <daniel@skillster.se>
2018-11-16 21:34:03 +0100

    Added a GravityWell prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Player Player = null;
+    public Camera mainCamera = null;
+    public Player player = null;
+    public GravityWell gravityWellPrefab = null;
+    public Transform gravityWellParent = null;
 
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
             Debug.Log("Clicked!");
+
+            Vector3 spawnPosition = mainCamera.ScreenPointToRay(Input.mousePosition).origin;
+            spawnPosition.z = 0f;
+
+            GravityWell well = Instantiate<GravityWell>(gravityWellPrefab, spawnPosition, Quaternion.identity, gravityWellParent);
+            well.AffectPlayer(player);
         }
     }
 
