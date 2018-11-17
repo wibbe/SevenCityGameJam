06fa14762f6f397dc81b733ce9e8a1ff146f0ac6
Daniel Wiberg <daniel@skillster.se>
2018-11-17 10:09:36 +0100

    Added post processing

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    public float tumbleSpeed = 0.4f;
+
+    private Quaternion m_rotation;
+
+    private void Start()
+    {
+        m_rotation = Random.rotationUniform;
+    }
+
+    private void Update()
+    {
+        //transform.rotation = transform.rotation * m_rotation * tumbleSpeed * Time.deltaTime;
+    }
+}
