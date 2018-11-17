a432418d4c653ce2513c4ca483e67eeb0e1dfb8c
Fricken <victor.j.frick@gmail.com>
2018-11-17 15:42:49 +0100

    Change player trail

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -50,7 +50,7 @@ public class Player : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Pickup") && Vector3.Distance(transform.position, other.transform.position) < 4.0f)
+        if (other.gameObject.CompareTag("Pickup") && Vector3.Distance(transform.position, other.transform.position) < 4.0f * energy / 10.0f)
         {
             Debug.Log(Vector3.Distance(transform.position, other.transform.position));
             energy += 5f;
