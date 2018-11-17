082e9269f3a38b7c924fd9b7da8538773f9fda4b
Daniel Wiberg <daniel@skillster.se>
2018-11-17 22:00:33 +0100

    Worked on camera shake effect.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public void Shake()
+    {
+        StartCoroutine(ShakeEffect());
+    }
+
+    private IEnumerator ShakeEffect()
+    {
+        return null;
+    }
+}
