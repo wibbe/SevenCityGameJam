2366c1e37f24bcc86c0b283addb71091f76ef82c
Daniel Wiberg <daniel@skillster.se>
2018-11-17 09:33:27 +0100

    Some tweakes

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -6,23 +6,21 @@ public class FollowObject: MonoBehaviour
 {
     public Transform target = null;
     public float dragTime = 2f;
-
-    private float z = 0f;
+    public float zHeight = -100f;
 
     private void Start()
     {
-        z = transform.position.z;
     }
 
     private void Update()
     {
         
         Vector3 newPos = Vector3.Lerp(transform.position, target.position, dragTime * Time.deltaTime);
-        newPos.z = z;
+        newPos.z = zHeight;
         transform.position = newPos;
 
         //Vector3 newRotation = Vector3.Lerp(transform.eulerAngles, target.eulerAngles, dragTime * Time.deltaTime);
-        Debug.Log(Vector3.Angle(target.GetComponent<Rigidbody>().velocity.normalized, Vector3.up));
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector3.Angle(target.GetComponent<Rigidbody>().velocity.normalized, Vector3.up)));
+        //Debug.Log(Vector3.Angle(target.GetComponent<Rigidbody>().velocity.normalized, Vector3.up));
+        //transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector3.Angle(target.GetComponent<Rigidbody>().velocity.normalized, Vector3.up)));
     }
 }
