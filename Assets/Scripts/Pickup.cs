4825bc86d24c102f8e3f6b2ca8294491b0961830
Daniel Wiberg <daniel@skillster.se>
2018-11-17 23:24:33 +0100

    Scale down pickups when the come close to the player.

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -10,19 +10,24 @@ public class Pickup : MonoBehaviour
     
     private Rigidbody m_rigidbody;
     private float pullTime = 2.0f;
+    private float initialScale = 1f;
     
 
     private void Start()
     {
+        initialScale = transform.localScale.x;
         m_rigidbody = GetComponent<Rigidbody>();
         m_rigidbody.AddTorque(new Vector3(Random.Range(-rotationSpeed, rotationSpeed), Random.Range(-rotationSpeed, rotationSpeed), Random.Range(-rotationSpeed, rotationSpeed)));
     }
 
     private void Update()
     {
-        if(target != null)
+        if (target != null)
         {
             transform.position = Vector3.Lerp(transform.position, target.position, 2.0f - pullTime);
+
+            float scale = Mathf.Clamp01(pullTime / 2f) * initialScale;
+            transform.localScale = new Vector3(scale, scale, scale);
             pullTime -= Time.deltaTime;
         }
     }
